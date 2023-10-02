aead03f13250ab44f413d69b78287874b93775b5
Abubakar Bunza Mubarak <mubarakabubakar227@gmail.com>
2023-10-02 22:58:14 +0000

    #14 migrate local state to remote state using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "mab_learning"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
